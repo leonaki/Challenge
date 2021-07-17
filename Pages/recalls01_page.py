95e239e418b54f9be9d3e4d87d1f0d5b0a57ff8a
Leon <laquinocervantes@gmail.com>
2021-07-17 14:22:59 -0400

    Push 3

--- Pages/recalls01_page.py
+++ Pages/recalls01_page.py
@@ -18,6 +18,24 @@ class Recalls01Page(BasePage):
     def get_disclaimer_how_it_works(self):
         return self.find_element_by_xpath('/html/body/div[1]/div/div/a/strong')
 
+    def click_disclaimer_how_it_works(self):
+        return self.find_element_by_css_selector('#advertising-disclosure > div > div > a').click()
+
+    def get_footer_contactus(self):
+        return self.find_element_by_xpath('/html/body/footer/div[2]/div[2]/div/a[1]')
+
+    def get_footer_faq(self):
+        return self.find_element_by_xpath('/html/body/footer/div[2]/div[2]/div/a[2]')
+
+    def get_footer_terms_of_use(self):
+        return self.find_element_by_xpath('/html/body/footer/div[2]/div[2]/div/a[3]')
+
+    def get_footer_privacy_policy(self):
+        return self.find_element_by_xpath('/html/body/footer/div[2]/div[2]/div/a[4]')
+
+    def get_footer_ca_notice(self):
+        return self.find_element_by_xpath('/html/body/footer/div[2]/div[2]/div/a[5]')
+
     def get_footer_body(self):
         return self.find_element_by_xpath('/html/body/footer/div[2]/div[1]/p[1]')
 
@@ -25,27 +43,16 @@ class Recalls01Page(BasePage):
         return self.find_element_by_xpath('/html/body/footer/div[2]/div[1]/p[2]')
 
     def get_social_facebook(self):
-        return self.find_element_by_xpath('/html/body/main/div[3]/article/div[2]/a[1]/svg/g/path[2]')
-
-    def click_social_facebook(self):
-        self.find_element_by_css_selector('#wrpr > div.h-sect.h-cont.h-col.h-col--invert > article > div:nth-child(6) > '
-                                          'a:nth-child(1) > svg > g > path.ca-icon__colored-fill').click()
+        return self.find_element_by_xpath('//*[@id="wrpr"]/div[3]/article/div[2]/a[1]')
 
     def get_social_twitter(self):
-        return self.find_element_by_xpath('/html/body/main/div[3]/article/div[2]/a[1]/svg/g/path[2]')
-
-    def click_social_twitter(self):
-        self.find_element_by_css_selector('#wrpr > div.h-sect.h-cont.h-col.h-col--invert > article > div:nth-child(6) >'
-                                          ' a:nth-child(2) > svg > g > path.ca-icon__colored-fill-2').click()
+        return self.find_element_by_xpath('//*[@id="wrpr"]/div[3]/article/div[2]/a[2]')
 
     def get_social_email(self):
         return self.find_element_by_css_selector('#wrpr > div.h-sect.h-cont.h-col.h-col--invert > article > div:nth-child(6) > a:nth-child(3)')
 
     def get_social_email_to(self):
         return self.find_element_by_css_selector('#wrpr > div.h-sect.h-cont.h-col.h-col--invert > article > div:nth-child(6) > a:nth-child(3)').get_attribute("href").partition("subject")[0][7:]
-
-    def click_social_email(self):
-        self.find_element_by_css_selector('#wrpr > div.h-sect.h-cont.h-col.h-col--invert > article > div:nth-child(6) > a:nth-child(3)').click()
 
     def set_find_my_match(self, message):
         self.find_element_by_xpath('/html/body/main/div[3]/article/div[5]/div/form/div[2]/div/div/input').send_keys(message)
@@ -60,12 +67,12 @@ class Recalls01Page(BasePage):
         self.find_element_by_css_selector('#wrpr > div.h-sect.h-cont.h-col.h-col--invert > article > div:nth-child(9) > div > form > div.wzrd-wg__form-wpr > button').click()
 
     def get_latest_news(self):
-        return self.find_element_by_xpath('/html/body/main/div[3]/aside/nav[2]/h3')
+        return self.find_elements_by_xpath('//*[@id="sidebar"]/nav[2]')
         # /html/body/main/div[3]/aside/nav[2]/a[last()]
         # /html/body/main/div[3]/aside/nav[2]/a[8]
 
     def get_related_new_stories(self):
-        return self.find_element_by_xpath('/html/body/main/div[3]/aside/nav[1]/h3')
+        return self.find_elements_by_xpath('//*[@id="sidebar"]/nav[1]/h3')
 
     def click_latest_news_first(self):
         self.find_element_by_xpath('//*[@id="sidebar"]/nav[2]/a[1]/div[1]').click()
@@ -74,9 +81,8 @@ class Recalls01Page(BasePage):
         self.find_element_by_xpath('//*[@id="sidebar"]/nav[2]/a[last()]/div[1]').click()
 
     def click_related_new_stories_first(self):
-        self.find_element_by_xpath('//*[@id="sidebar"]/nav[1]/a[1]/div[1]').click()
+        self.find_element_by_xpath('//*[@id="sidebar"]/nav[1]/a[1]/div[2]').click()
 
     def click_related_new_stories_last(self):
-        self.find_element_by_xpath('//*[@id="sidebar"]/nav[1]/a[last()]/div[1]').click()
-
+        self.find_element_by_xpath('//*[@id="sidebar"]/nav[1]/a[last()]/div[2]').click()
 
--- Tests/test_recalls01.py
+++ Tests/test_recalls01.py
@@ -1,6 +1,9 @@
 import pytest
 from time import sleep
 from selenium.webdriver import Chrome
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.remote.webelement import WebElement
+
 from Config import config
 from Pages.recalls01_page import Recalls01Page
 from selenium.common.exceptions import WebDriverException
@@ -9,7 +12,6 @@ from selenium.common.exceptions import WebDriverException
 @pytest.fixture
 def browser():
     driver = Chrome(config.TestData.CHROME_PATH)
-    #sleep(5)
     yield driver
     sleep(5)
     driver.quit()
@@ -19,135 +21,241 @@ def test_verify_disclaimer(browser):
     contact_page = Recalls01Page(browser)
     contact_page.open()
     print("TC01 - Verifying the disclaimer text")
-    assert config.TestData.disclaimerexpectedtext == Recalls01Page.get_disclaimer(browser).text, "Not valid disclaimer text"
-    print("Disclaimer text is accurate")
+    assert config.TestData.disclaimerexpectedtext == Recalls01Page.get_disclaimer(browser).text, "Incorrect text"
+    print("     Disclaimer text is accurate")
 
 
 def test_disclaimer_how_it_works(browser):
+    contact_page = Recalls01Page(browser)
+    contact_page.open()
     print("TC02 - Clicking on disclaimer")
     try:
-        Recalls01Page.get_disclaimer_how_it_works.click()
-        print("Disclaimer link is working fine")
+        Recalls01Page.click_disclaimer_how_it_works(browser)
+        print("     Disclaimer link is working fine")
     except WebDriverException:
-        print("Element is not clickable")
+        print("     Element is not clickable")
+        pytest.fail()
+
+
+def test_verify_footer_contactus(browser):
+    contact_page = Recalls01Page(browser)
+    contact_page.open()
+    print("TC03 - Footer Contact Us")
+    sleep(5)
+    assert config.TestData.contactusurl == Recalls01Page.get_footer_contactus(browser).get_attribute("href"), "Incorrect URL"
+    print("     03.1 - Footer Contact us link is accurate")
+    try:
+        sleep(5)
+        Recalls01Page.get_footer_contactus(browser).click()
+        sleep(5)
+        print("     03.2 - Footer Contact us link is working fine")
+    except WebDriverException:
+        print("     Element is not clickable")
+        pytest.fail()
+
+
+def test_verify_footer_faq(browser):
+    contact_page = Recalls01Page(browser)
+    contact_page.open()
+    print("TC04 - Footer Faq Us")
+    sleep(5)
+    assert config.TestData.faqurl == Recalls01Page.get_footer_faq(browser).get_attribute("href"), "Incorrect URL"
+    print("     04.1 - Footer Faq link is accurate")
+    try:
+        sleep(5)
+        Recalls01Page.get_footer_faq(browser).click()
+        sleep(5)
+        print("     04.2 - Footer Faq link is working fine")
+    except WebDriverException:
+        print("     Element is not clickable")
+        pytest.fail()
+
+
+def test_verify_footer_terms_of_use(browser):
+    contact_page = Recalls01Page(browser)
+    contact_page.open()
+    print("TC05 - Footer Terms of Use ")
+    sleep(5)
+    assert config.TestData.terms_of_useurl == Recalls01Page.get_footer_terms_of_use(browser).get_attribute("href"), "Incorrect URL"
+    print("     05.1 - Footer Terms of Use link is accurate")
+    try:
+        sleep(5)
+        Recalls01Page.get_footer_terms_of_use(browser).click()
+        sleep(5)
+        print("     05.2 - Footer Terms of Use link is working fine")
+    except WebDriverException:
+        print("     Element is not clickable")
+        pytest.fail()
+
+
+def test_verify_footer_privacy_policy(browser):
+    contact_page = Recalls01Page(browser)
+    contact_page.open()
+    print("TC06 - Footer Privacy Policy")
+    sleep(5)
+    assert config.TestData.privacypolicyurl == Recalls01Page.get_footer_privacy_policy(browser).get_attribute("href"), "Incorrect URL"
+    print("     06.1 - Footer Privacy Policy link is accurate")
+    try:
+        sleep(5)
+        Recalls01Page.get_footer_privacy_policy(browser).click()
+        sleep(5)
+        print("     06.2 - Footer Privacy Policy link is working fine")
+    except WebDriverException:
+        print("     Element is not clickable")
+        pytest.fail()
+
+
+def test_verify_footer_ca_notice(browser):
+    contact_page = Recalls01Page(browser)
+    contact_page.open()
+    print("TC07 - Footer CA Privacy Notice")
+    sleep(5)
+    assert config.TestData.canoticeurl == Recalls01Page.get_footer_ca_notice(browser).get_attribute("href"), "Incorrect URL"
+    print("     07.1 - CA Privacy Notice link is accurate")
+    try:
+        sleep(5)
+        Recalls01Page.get_footer_ca_notice(browser).click()
+        sleep(5)
+        print("     07.2 - Footer CA Privacy Notice link is working fine")
+    except WebDriverException:
+        print("     Element is not clickable")
+        pytest.fail()
 
 
 def test_verify_footer_body(browser):
     contact_page = Recalls01Page(browser)
     contact_page.open()
-    print("TC03 - Verifying the footer body text")
-    assert config.TestData.footerexpectedbody == Recalls01Page.get_footer_body(browser).text, "Not valid footer body text"
-    print("Footer body text is accurate")
+    print("TC08 - Verifying the footer body text")
+    assert config.TestData.footerexpectedbody == Recalls01Page.get_footer_body(browser).text, "     Not valid footer body text"
+    print("     Footer body text is accurate")
 
 
 def test_verify_footer_copyright(browser):
     contact_page = Recalls01Page(browser)
     contact_page.open()
-    print("TC04 - Verifying the footer copyright text")
-    assert config.TestData.footerexpectedcopyright == Recalls01Page.get_footer_copyright(browser).text, "Not valid footer copyright text"
-    print("Footer copyright text is accurate")
+    print("TC09 - Verifying the footer copyright text")
+    assert config.TestData.footerexpectedcopyright == Recalls01Page.get_footer_copyright(browser).text, "       Not valid footer copyright text"
+    print("     Footer copyright text is accurate")
 
 
 def test_social_facebook(browser):
     contact_page = Recalls01Page(browser)
     contact_page.open()
-    print("TC05 - Click on Facebook social link")
+    print("TC10 - Click on Facebook social link")
     try:
-        Recalls01Page.click_social_facebook(browser)
         sleep(5)
-        print("Facebook link is working fine")
+        Recalls01Page.get_social_facebook(browser).click()
+        sleep(5)
+        print("     Facebook link is working fine")
     except WebDriverException:
-        print("Element is not clickable")
+        print("     Element is not clickable")
+        pytest.fail()
 
 
 def test_social_twitter(browser):
     contact_page = Recalls01Page(browser)
     contact_page.open()
-    print("TC06 - Click on Twitter social link")
+    print("TC11 - Click on Twitter social link")
+    assert config.TestData.twitterurl in Recalls01Page.get_social_twitter(browser).get_attribute("href"), "Not valid URL"
+
     try:
-        Recalls01Page.click_social_twitter(browser)
         sleep(5)
-        print("Twitter link is working fine")
+        Recalls01Page.get_social_twitter(browser).click()
+        sleep(5)
+        print("     Twitter Social link is working fine")
     except WebDriverException:
         print("Element is not clickable")
+        pytest.fail()
 
 
 def test_social_email(browser):
     contact_page = Recalls01Page(browser)
     contact_page.open()
-    print("TC07 - Test Email social link")
+    print("TC12 - Test Email social link")
     try:
-        Recalls01Page.click_social_email(browser)
         sleep(5)
-        print("Email link is working fine")
+        Recalls01Page.get_social_email(browser).click()
+        sleep(5)
+        print("     Email link is working fine")
     except WebDriverException:
         print("Element is not clickable")
-
-    assert config.TestData.socialmailto == Recalls01Page.get_social_email_to(browser), "Blank or Invalid Email To"
+        pytest.fail()
 
 
 def test_find_my_match(browser):
     contact_page = Recalls01Page(browser)
     contact_page.open()
     print("TC08 - Find my match box")
+
     Recalls01Page.set_find_my_match(browser, '34744')
 
     assert Recalls01Page.get_find_my_match(browser), "Find my match textbox is blank"
+
     try:
-        Recalls01Page.click_find_my_match_button(browser)
+        sleep(5)
+        Recalls01Page.get_find_my_match_button(browser).click()
         sleep(5)
         print("Find my match button is working fine")
     except WebDriverException:
         print("Element is not clickable")
+        pytest.fail()
 
 
 def test_latest_news_first(browser):
     contact_page = Recalls01Page(browser)
     contact_page.open()
-    print("TC09 - Click on First Latest News")
+    print("TC13 - Click on First Latest News")
 
     try:
+        sleep(5)
         Recalls01Page.click_latest_news_first(browser)
         sleep(5)
-        print("First listed Latest News exists")
+        print("     First listed Latest News exists")
     except WebDriverException:
         print("Element not found")
+        pytest.fail()
 
 
 def test_latest_news_last(browser):
     contact_page = Recalls01Page(browser)
     contact_page.open()
-    print("TC10 - Click on Last Latest News")
+    print("TC14 - Click on Last Latest News")
 
     try:
+        sleep(5)
         Recalls01Page.click_latest_news_last(browser)
         sleep(5)
-        print("Last listed Latest News exists")
+        print("     Last listed Latest News exists")
     except WebDriverException:
         print("Element not found")
+        pytest.fail()
 
 
-def test_related_new_stories_first(browser):
+def test_related_news_first(browser):
     contact_page = Recalls01Page(browser)
     contact_page.open()
-    print("TC11 - Click on First New Stories")
+    print("TC15 - Click on First Related News")
 
     try:
-        Recalls01Page.click_latest_news_first(browser)
         sleep(5)
-        print("First listed New Stories exists")
+        Recalls01Page.click_related_new_stories_first(browser)
+        sleep(5)
+        print("     First listed Related News exists")
     except WebDriverException:
-        print("Element not found")
+        print("     Element not found")
+        pytest.fail("Failed")
 
 
-def test_related_new_stories_last(browser):
+def test_related_news_last(browser):
     contact_page = Recalls01Page(browser)
     contact_page.open()
-    print("TC12 - Click on Last New Stories")
+    print("TC16 - Click on Last Related News")
 
     try:
-        Recalls01Page.click_latest_news_last(browser)
         sleep(5)
-        print("Last listed New Stories exists")
+        Recalls01Page.click_related_new_stories_last(browser)
+        sleep(5)
+        print("     Last listed Related News exists")
     except WebDriverException:
-        print("Element not found")
+        print("     Element not found")
+        pytest.fail("Failed")
